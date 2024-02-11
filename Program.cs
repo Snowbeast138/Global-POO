1ea90ae19a2569ba66d274353d4dda6843f924e1
Snowbeast138 <snowbeast138@gmail.com>
2024-02-10 23:52:34 -0600

    Show Pokedex

--- Program.cs
+++ Program.cs
@@ -1,9 +1,83 @@
-﻿using static System.Console;
+﻿using System.Text.Json;
+
+using static System.Console;
+
+public class Pokemon
+{
+    public int id { get; set; }
+
+    public name name { get; set; }
+
+    public List<string> type { get; set; }
+
+    public BaseStats BaseStats { get; set; }
+}
+
+public class name
+{
+    public string english { get; set; }
+}
+
+public class BaseStats
+{
+    public int HP { get; set; }
+
+    public int Attack { get; set; }
+
+    public int Defense { get; set; }
+
+    public int SpAttack { get; set; }
+
+    public int SpDefense { get; set; }
+
+    public int Speed { get; set; }
+}
 
 class Program
 {
     static void Main()
     {
-        WriteLine("Hola mundo");
+        string path = "./Pokedex.Json";
+
+        if (File.Exists(path))
+        {
+            string jsonString = File.ReadAllText(path);
+
+            List<Pokemon> pokemons =
+                JsonSerializer.Deserialize<List<Pokemon>>(jsonString);
+
+            ShowPokedex (pokemons);
+        }
+        else
+        {
+            WriteLine("El archivo Pokedex.Json no existe.");
+        }
+    }
+
+    static void ShowPokedex(List<Pokemon> pokemons)
+    {
+        foreach (var pokemon in pokemons)
+        {
+            WriteLine("----------------------------------");
+            WriteLine($"ID: {pokemon.id}");
+
+            WriteLine($"Pokemon: {pokemon.name.english}");
+
+            WriteLine("Tipos:");
+            foreach (var type in pokemon.type)
+            {
+                WriteLine($"- {type}");
+            }
+            // WriteLine();
+            // WriteLine("Estadísticas base:");
+            // WriteLine($"HP: {pokemon.BaseStats.HP}");
+            // WriteLine($"Ataque: {pokemon.BaseStats.Attack}");
+            // WriteLine($"Defensa: {pokemon.BaseStats.Defense}");
+            // WriteLine($"Ataque Especial: {pokemon.BaseStats.SpAttack}");
+            // WriteLine($"Defensa Especial: {pokemon.BaseStats.SpDefense}");
+            // WriteLine($"Velocidad: {pokemon.BaseStats.Speed}");
+            // WriteLine("----------------------------------");
+            // WriteLine();
+        }
     }
 }
